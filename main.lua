1cea46e1ff0e766d696a40fc9b29f041a4325bfc
Alexander <freefallerr@gmail.com>
2023-07-22 17:13:47 +0100

    this wont work

--- main.lua
+++ main.lua
@@ -1,154 +1,20 @@
-local http = require("socket.http")
+local args = require("args")
+local http_request = require("http_request")
 local url = require("socket.url")
-local ltn12 = require("ltn12")
-
-local function parseArgs(args)
-    local result = {}
-    local i = 1
-
-    while i <= #args do
-        local argName = args[i]
-        local argValue = args[i + 1]
-
-        if argName and argName:sub(1, 2) == "--" then
-            argName = argName:sub(3)
-            result[argName] = argValue
-            i = i + 2
-        elseif argName and argName:sub(1, 1) == "-" then
-            argName = argName:sub(2)
-            result[argName] = argValue
-            i = i + 2
-        else
-            i = i + 1
-        end
-    end
-
-    return result
-end
-
-local function processArgs(args)
-    local defaultArgs = {
-        threads = 10,
-        port = 80,
-        status_codes = 200
-    }
-
-    local namedArgs = parseArgs(args)
-
-    for key, value in pairs(defaultArgs) do
-        namedArgs[key] = namedArgs[key] or value
-    end
-
-    return namedArgs
-end
-
-local function printHelp()
-    print("Usage: lua lirb.lua -url <url> -wl <path> [options]")
-    print("  --target or -t url                        : Target URL")
-    print("  --wordlist or -w path                     : Path to wordlist")
-    print("Options:")
-    print("  --character-count or -cc int              : Character count of the response to filter")
-    print("  --cookies or -c test=abc;token=xyz        : Add cookies to the requests")
-    print("  --headers or -h Authorization Bearer 123  : Add custom headers to the requests. Use this for Authorization tokens")
-    print("  --threads or -T int                       : How many requests can be sent in parallel")
-    print("  --proxy or -P http://127.0.0.1:8080       : Add proxy")
-    print("  --port or -p int                          : Add port")
-    print("  --status-codes or -sc int,int,...         : Comma-separated list of status codes to whitelist")
-    print("  --user-agent or -ua string                : Custom user-agent to use for requests")
-end
-
-local function makeRequest(params)
-    local request_headers = {}
-
-    if params.headers then
-        for header in params.headers:gmatch("([^;]+)") do
-            local key, value = header:match("([^:]+):%s*(.+)")
-            if key and value then
-                request_headers[key] = value
-            end
-        end
-    end
-
-    if params.cookies then
-        request_headers["Cookie"] = params.cookies
-    end
-
-    if params.user_agent then
-        request_headers["User-Agent"] = params.user_agent
-    end
-
-    local response_body = {}
-    local _, status_code, response_headers, status_text = http.request{
-        url = params.target,
-        method = "GET",
-        headers = request_headers,
-        proxy = params.proxy,
-        port = params.port,
-        sink = ltn12.sink.table(response_body)
-    }
-
-    local response = table.concat(response_body)
-
-    return response, status_code, status_text
-end
-
-local function getFullURL(baseURL, wordlistPath)
-    local fullURLs = {}
-
-    if baseURL:sub(-1) ~= "/" then
-        baseURL = baseURL .. "/"
-    end
-
-    local file = io.open(wordlistPath, "r")
-    if not file then
-        print("Error: Unable to open wordlist file")
-        return fullURLs
-    end
-
-    for line in file:lines() do
-        line = line:match("^%s*(.-)%s*$")
-        local fullURL = baseURL .. line
-        table.insert(fullURLs, fullURL)
-    end
-
-    file:close()
-    return fullURLs
-end
-
-local function checkStatusCode(status_code, status_codes)
-    if not status_codes then
-        status_codes = {200}
-    end
-
-    for _, code in ipairs(status_codes) do
-        if tonumber(code) == tonumber(status_code) then
-            return true
-        end
-    end
-
-    return false
-end
-
-local function processRequest(params, valid_urls)
-    local response, status_code, status_text = makeRequest(params)
-    if checkStatusCode(status_code, params.status_codes) then
-        if not params.character_count or #response ~= params.character_count then
-            io.write(string.format("\r%s - Status Code: %s, Response Length: %d\n", params.target, status_code, #response))
-            table.insert(valid_urls, {url = params.target, status = status_code, response = response})
-        end
-    end
-end
+local utils = require("lua.lirb.utils")
 
 local function runRequests(params)
-    local full_urls = getFullURL(params.target, params.wl)
-    
+    local full_urls = utils.getFullURL(params.target, params.wl)
     local valid_urls = {}
 
     for i, full_url in ipairs(full_urls) do
         io.write(string.format("\rProgress: %d / %d", i, #full_urls))
         io.flush()
-        params.target = full_url
-        processRequest(params, valid_urls)
+
+        local request_params = http_request.table_copy(params)
+        request_params.target = full_url
+
+        http_request.processRequest(request_params, valid_urls)
     end
 
     return valid_urls
@@ -156,7 +22,7 @@ end
 
 
 local function main()
-    local namedArgs = processArgs(arg)
+    local namedArgs = args.processArgs(arg)
 
     local params = {
         target = namedArgs["target"] or namedArgs["t"],
@@ -207,12 +73,12 @@ local function main()
         print("Finished")
         print("=====================================================\n")
     else
-        printHelp()
+        args.printHelp()
     end
 end
 
 if arg[1] == "--help" then
-    printHelp()
+    args.printHelp()
 else
     main()
 end
